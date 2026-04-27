e706a214e8b190c1d50d587484242cbcc8a13780
edoardoscudeller <edoardo.scudeller0@gmail.com>
2026-04-27 11:47:06 +0200

    Add EnumerationWLOpt class definition

--- WL_Enumeration.hh
+++ WL_Enumeration.hh
@@ -2,3 +2,14 @@
 #include "WL_Data.hh"
 
 // insert here the definition of the class EnumerationWLOpt
+
+class EnumerationWLOpt : public EnumerationOpt<WL_Input, WL_Output, unsigned>
+{
+public:
+  EnumerationWLOpt(const WL_Input& in) : EnumerationOpt(in) {}
+protected:
+  void First() override;
+  bool Next() override;
+  bool Feasible() override;
+  unsigned Cost() override;
+};
